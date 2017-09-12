34b1e40eb3aadc979b9b2de55b91d5cba1b54640
HsiangHo <ho_cheung@qq.com>
2017-09-13 02:30:29 +0800

    Initialize

---
+++ YuYinZhong/YuYinZhong/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  YuYinZhong
+//
+//  Created by Jovi on 9/13/17.
+//  Copyright © 2017 Jovi. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ YuYinZhong/YuYinZhong/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  YuYinZhong
+//
+//  Created by Jovi on 9/13/17.
+//  Copyright © 2017 Jovi. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
