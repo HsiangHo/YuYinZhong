70f0850e7c46e6fc5afc77bb9924ea6c42c59939
HsiangHo <ho_cheung@qq.com>
2017-09-15 01:34:12 +0800

    implement the scheduled announce

--- YuYinZhong/YuYinZhong/HAFVoiceClockManager.m
+++ YuYinZhong/YuYinZhong/HAFVoiceClockManager.m
@@ -18,6 +18,9 @@ static HAFVoiceClockManager *instance;
     HAF_Announce_Format_Type        _announceType;
     HAF_Announce_Rule               _announceRule;
     BOOL                            _twentyfourHour;
+    NSTimer                         *_taskDistributorTimer;
+    NSTimer                         *_taskerTimer;
+    NSDate                          *_nextFireDate;
 }
 
 +(instancetype)sharedManager{
@@ -36,16 +39,59 @@ static HAFVoiceClockManager *instance;
     return self;
 }
 
+-(void)announceThisTimeUsingMandarin{
+    [_mandarinClock set24HourClock:_twentyfourHour];
+    [_player startPlaying:[_mandarinClock announceThisTime:_announceType] withBundle:_bundleMandarin];
+}
+
 -(void)__initializeHAFVoiceClockManager{
     _player = [[HAFAudioPlayer alloc] init];
     _mandarinClock = [[HAFMandarinVoiceClock alloc] init];
     NSString *path = [[NSBundle mainBundle] pathForResource:@"vc-Zh-Hans" ofType:@"bundle"];
     _bundleMandarin = [NSBundle bundleWithPath:path];
+    _taskDistributorTimer = [NSTimer scheduledTimerWithTimeInterval:60 target:self selector:@selector(__distributeAnnounceTask) userInfo:nil repeats:YES];
 }
 
--(void)announceThisTimeUsingMandarin{
-    [_mandarinClock set24HourClock:_twentyfourHour];
-    [_player startPlaying:[_mandarinClock announceThisTime:_announceType] withBundle:_bundleMandarin];
+-(void)__distributeAnnounceTask{
+    NSDate *date = [self __dateToNearest15Minutes];
+    if(![date isEqualToDate:_nextFireDate]){
+        _nextFireDate = date;
+        _taskerTimer = [[NSTimer alloc] initWithFireDate:_nextFireDate interval:0 target:self selector:@selector(__taskAction) userInfo:nil repeats:NO];
+        [[NSRunLoop currentRunLoop] addTimer:_taskerTimer forMode:NSDefaultRunLoopMode];
+    }
+}
+
+-(void)__taskAction{
+    BOOL bAnnounce = NO;
+    NSDateComponents *comps = [[NSCalendar currentCalendar] components:NSMinuteCalendarUnit fromDate:[NSDate date]];
+    switch (_announceRule) {
+        case eHAF_Announce_Hourly:
+            bAnnounce = (0 == comps.minute);
+            break;
+            
+        case eHAF_Announce_Half_Hourly:
+            bAnnounce = (0 == comps.minute % 30);
+            break;
+            
+        case eHAF_Announce_Quarter_Hourly:
+            bAnnounce = (0 == comps.minute % 15);
+            break;
+            
+        case eHAF_Announce_By_Hand:
+        default:
+            break;
+    }
+    if (bAnnounce) {
+        [self announceThisTimeUsingMandarin];
+    }
+}
+
+- (NSDate *)__dateToNearest15Minutes {
+    unsigned unitFlags = NSYearCalendarUnit| NSMonthCalendarUnit | NSDayCalendarUnit | NSWeekCalendarUnit |  NSHourCalendarUnit | NSMinuteCalendarUnit | NSSecondCalendarUnit | NSWeekdayCalendarUnit | NSWeekdayOrdinalCalendarUnit;
+    NSDateComponents *comps = [[NSCalendar currentCalendar] components:unitFlags fromDate:[NSDate date]];
+    [comps setMinute:((([comps minute] - 8 ) / 15 ) * 15 ) + 15];
+    [comps setSecond:0];
+    return [[NSCalendar currentCalendar] dateFromComponents:comps];
 }
 
 @end
