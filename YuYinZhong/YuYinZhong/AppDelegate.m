b65633bb627a7a25e63e5c2df93d24c47b945f15
HsiangHo <ho_cheung@qq.com>
2017-09-16 13:34:18 +0800

    localize menu string

--- YuYinZhong/YuYinZhong/AppDelegate.m
+++ YuYinZhong/YuYinZhong/AppDelegate.m
@@ -9,6 +9,8 @@
 #import "AppDelegate.h"
 #import "HAFVoiceClockManager.h"
 #import "ViewControllerManager.h"
+#import <XUIKit/XUIKit.h>
+#import "define.h"
 
 @interface AppDelegate ()
 
@@ -17,46 +19,69 @@
 
 @implementation AppDelegate{
     NSStatusItem                    *_statusItem;
+    NSMenuItem                      *_menuItemAnnounce;
+    NSMenuItem                      *_menuItemAbout;
+    NSMenuItem                      *_menuItemPreferences;
+    NSMenuItem                      *_menuItemRate;
+    NSMenuItem                      *_menuItemTellFriends;
+    NSMenuItem                      *_menuItemHelp;
+    NSMenuItem                      *_menuItemQuit;
 }
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
-    NSMenu *menu = [[NSMenu alloc] initWithTitle:@"Yuyinzhong-menu"];
-    NSMenuItem *menuItemAnnounce = [[NSMenuItem alloc] initWithTitle:@"Announce" action:@selector(announce_click:) keyEquivalent:@"a"];
-    [menuItemAnnounce setTarget:self];
-    NSMenuItem *menuItemAbout = [[NSMenuItem alloc] initWithTitle:@"About" action:@selector(about_click:) keyEquivalent:@""];
-    [menuItemAbout setTarget:self];
-    NSMenuItem *menuItemPreferences = [[NSMenuItem alloc] initWithTitle:@"Preferences" action:@selector(preference_click:) keyEquivalent:@""];
-    [menuItemPreferences setTarget:self];
-    NSMenuItem *menuItemRate = [[NSMenuItem alloc] initWithTitle:@"Rate In App Store" action:@selector(rate_click:) keyEquivalent:@""];
-    [menuItemRate setTarget:self];
-    NSMenuItem *menuItemTellFriends = [[NSMenuItem alloc] initWithTitle:@"Tell a friend" action:@selector(tellFriends_click:) keyEquivalent:@""];
-    [menuItemTellFriends setTarget:self];
-    NSMenuItem *menuItemHelp = [[NSMenuItem alloc] initWithTitle:@"Help" action:@selector(help_click:) keyEquivalent:@""];
-    [menuItemHelp setTarget:self];
-    NSMenuItem *menuItemQuit = [[NSMenuItem alloc] initWithTitle:@"Quit" action:@selector(quit_click:) keyEquivalent:@"q"];
-    [menuItemQuit setTarget:self];
+    [[XUISharingManager sharedManager] setShareItems:[[NSMutableArray alloc] initWithObjects:SHARE_CONTENT, nil]];
     
-    [menu addItem:menuItemAnnounce];
+    NSMenu *menu = [[NSMenu alloc] initWithTitle:@"Yuyinzhong-menu"];
+    _menuItemAnnounce = [[NSMenuItem alloc] initWithTitle:@"" action:@selector(announce_click:) keyEquivalent:@"a"];
+    [_menuItemAnnounce setTarget:self];
+    _menuItemAbout = [[NSMenuItem alloc] initWithTitle:@"" action:@selector(about_click:) keyEquivalent:@""];
+    [_menuItemAbout setTarget:self];
+    _menuItemPreferences = [[NSMenuItem alloc] initWithTitle:@"" action:@selector(preference_click:) keyEquivalent:@","];
+    [_menuItemPreferences setTarget:self];
+    _menuItemRate = [[NSMenuItem alloc] initWithTitle:@"" action:@selector(rateApp_click:) keyEquivalent:@""];
+    [_menuItemRate setTarget:self];
+    _menuItemTellFriends = [[NSMenuItem alloc] initWithTitle:@"" action:@selector(tellFriends_click:) keyEquivalent:@""];
+    [_menuItemTellFriends setSubmenu:[[XUISharingManager sharedManager] sharingMenu]];
+    _menuItemHelp = [[NSMenuItem alloc] initWithTitle:@"" action:@selector(help_click:) keyEquivalent:@""];
+    [_menuItemHelp setTarget:self];
+    _menuItemQuit = [[NSMenuItem alloc] initWithTitle:@"" action:@selector(quit_click:) keyEquivalent:@"q"];
+    [_menuItemQuit setTarget:self];
+    
+    [menu addItem:_menuItemAnnounce];
     [menu addItem:[NSMenuItem separatorItem]];
-    [menu addItem:menuItemAbout];
-    [menu addItem:menuItemPreferences];
+    [menu addItem:_menuItemPreferences];
     [menu addItem:[NSMenuItem separatorItem]];
-    [menu addItem:menuItemRate];
-    [menu addItem:menuItemTellFriends];
-    [menu addItem:menuItemHelp];
+    [menu addItem:_menuItemAbout];
+    [menu addItem:_menuItemRate];
+    [menu addItem:_menuItemTellFriends];
+    [menu addItem:_menuItemHelp];
     [menu addItem:[NSMenuItem separatorItem]];
-    [menu addItem:menuItemQuit];
+    [menu addItem:_menuItemQuit];
     _statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:20];
     NSImage *image = [NSImage imageNamed:@"AppIcon"];
     [image setSize:NSMakeSize(18, 18)];
     [_statusItem setImage:image];
     [_statusItem setMenu:menu];
+    
+    [self __localizeString];
+    [[XUILanguageManager sharedManager] addLanguageChangedBlock:^{
+        [self __localizeString];
+    }];
 }
-
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
     // Insert code here to tear down your application
+}
+
+-(void)__localizeString{
+    [_menuItemAnnounce setTitle:XUILocalizedString(@"announce")];
+    [_menuItemAbout setTitle:XUILocalizedString(@"about: %@")];
+    [_menuItemPreferences setTitle:XUILocalizedString(@"preferences")];
+    [_menuItemRate setTitle:XUILocalizedString(@"rate_in_App_Store")];
+    [_menuItemTellFriends setTitle:XUILocalizedString(@"tell_a_friend")];
+    [_menuItemHelp setTitle:XUILocalizedString(@"help")];
+    [_menuItemQuit setTitle:XUILocalizedString(@"quit")];
 }
 
 -(IBAction)announce_click:(id)sender{
@@ -71,15 +96,15 @@
     [[ViewControllerManager instance] showPreferencesWindow];
 }
 
--(IBAction)rate_click:(id)sender{
-    
-}
-
--(IBAction)tellFriends_click:(id)sender{
-    
+-(IBAction)rateApp_click:(id)sender{
+    [[NSWorkspace sharedWorkspace] openURL:[NSURL URLWithString:@"macappstore://itunes.apple.com/app/id1286248614"]];
 }
 
 -(IBAction)help_click:(id)sender{
+    [[NSWorkspace sharedWorkspace] openURL:[NSURL URLWithString:@"https://hsiangho.github.io/"]];
+}
+
+-(IBAction)tellFriends_click:(id)sender{
     
 }
 
