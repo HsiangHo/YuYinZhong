a15e6f02774b1af86c62bde5d74f051d3543035b
HsiangHo <ho_cheung@qq.com>
2017-09-16 13:29:37 +0800

    added define.h

---
+++ YuYinZhong/YuYinZhong/define.h
@@ -0,0 +1,15 @@
+//
+//  define.h
+//  YuYinZhong
+//
+//  Created by Jovi on 9/16/17.
+//  Copyright © 2017 Jovi. All rights reserved.
+//
+
+#ifndef define_h
+#define define_h
+
+#define APP_NAME            @"语音钟"
+#define SHARE_CONTENT       @"最近我正在使用一款非常棒的App: 语音钟 - 中文语音报时. (语音超可爱喔！) 从Mac App Store获得: https://itunes.apple.com/app/id1286248614"
+
+#endif /* define_h */
