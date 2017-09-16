14bf8f48731e8d4652569b372c1748dbb8e2d99b
HsiangHo <ho_cheung@qq.com>
2017-09-16 12:32:01 +0800

    completed the PreferencesWindowController

--- YuYinZhong/YuYinZhong/PreferencesWindowController.m
+++ YuYinZhong/YuYinZhong/PreferencesWindowController.m
@@ -21,11 +21,22 @@
     XUIView                 *_mainView;
     XUICheckbox             *_cbLaunchAtLogin;
     XUICheckbox             *_cb24HourMode;
+    
+    XUILabel                *_lbAnnounceRuleString;
+    NSArray                 *_arrayAnnounceRules;
+    XUIComboBox             *_cbAnnounceRule;
+    
+    XUILabel                *_lbAnnounceTypeString;
+    NSArray                 *_arrayAnnounceTypes;
+    XUIComboBox             *_cbAnnounceType;
+    
     XUILabel                *_lbLanguageString;
     XUIComboBox             *_cbLanguage;
     NSArray                 *_arrayLanguages;
     NSArray                 *_arrayLanguagesCode;
 }
+
+#pragma mark - Override methods
 
 -(instancetype)init{
     if (self = [super initWithWindowNibName:@"PreferencesWindowController"]) {
@@ -33,6 +44,16 @@
     }
     return self;
 }
+
+-(void)awakeFromNib{
+    [self.window setBackgroundColor:[NSColor colorWithCalibratedRed:250/255.0 green:250/255.0 blue:250/255.0 alpha:0.99]];
+    [[self.window standardWindowButton:NSWindowMiniaturizeButton] setHidden:YES];
+    [[self.window standardWindowButton:NSWindowZoomButton] setHidden:YES];
+    [self.window setContentView:_mainView];
+    [_mainView layoutSubviews];
+}
+
+#pragma mark - Private methods
 
 -(void)__initializedPreferencesWindowController{
     _arrayLanguages = @[@"简体中文",@"繁體中文"];
@@ -48,16 +69,41 @@
     [_cbLaunchAtLogin setState:[[HAFConfigureManager sharedManager] isStartup]];
     [_mainView addSubview:_cbLaunchAtLogin];
     
+    _lbAnnounceRuleString = [[XUILabel alloc] initWithFrame:NSMakeRect(0, 0, 100, 23)];
+    [_lbAnnounceRuleString setTextColor:TITLE_COLOR];
+    [_lbAnnounceRuleString setFont:TITLE_FONT];
+    [_mainView addSubview:_lbAnnounceRuleString];
+    
+    _cbAnnounceRule = [[XUIComboBox alloc] initWithFrame:NSMakeRect(0, 0, 200, 26)];
+    [_mainView addSubview:_cbAnnounceRule];
+    [_cbAnnounceRule setTextColor:TITLE_COLOR];
+    [_cbAnnounceRule setDelegate:(id<NSComboBoxDelegate>)self];
+    
+    _lbAnnounceTypeString = [[XUILabel alloc] initWithFrame:NSMakeRect(0, 0, 100, 23)];
+    [_lbAnnounceTypeString setTextColor:TITLE_COLOR];
+    [_lbAnnounceTypeString setFont:TITLE_FONT];
+    [_mainView addSubview:_lbAnnounceTypeString];
+    
+    _arrayAnnounceTypes = @[@"HH:mm",@"MM-dd HH:mm",@"WD HH:mm",@"MM-dd WD HH:mm",@"yyyy-MM-dd WD HH:mm"];
+    _cbAnnounceType = [[XUIComboBox alloc] initWithFrame:NSMakeRect(0, 0, 200, 26)];
+    [_mainView addSubview:_cbAnnounceType];
+    [_cbAnnounceType setTextColor:TITLE_COLOR];
+    for(NSString *style in _arrayAnnounceTypes){
+        [_cbAnnounceType addItemWithObjectValue:style];
+    }
+    [_cbAnnounceType selectItemAtIndex:[[HAFConfigureManager sharedManager] announceType]];
+    [_cbAnnounceType setDelegate:(id<NSComboBoxDelegate>)self];
+    
     _cb24HourMode = [[XUICheckbox alloc] initWithFrame:NSMakeRect(0, 0, 420, 30)];
     [_cb24HourMode setTitleColor:TITLE_COLOR];
     [_cb24HourMode setFont:TITLE_FONT];
     [_cb24HourMode setDetaY:3];
     [_cb24HourMode setTarget:self];
-    [_cb24HourMode setAction:@selector(autoBlock_click:)];
+    [_cb24HourMode setAction:@selector(twentyfourMode_click:)];
     [_cb24HourMode setState:[[HAFConfigureManager sharedManager] isTwentyfourHour]];
     [_mainView addSubview:_cb24HourMode];
     
-    _lbLanguageString = [[XUILabel alloc] initWithFrame:NSMakeRect(0, 0, 80, 23)];
+    _lbLanguageString = [[XUILabel alloc] initWithFrame:NSMakeRect(0, 0, 100, 23)];
     [_lbLanguageString setTextColor:TITLE_COLOR];
     [_lbLanguageString setFont:TITLE_FONT];
     [_mainView addSubview:_lbLanguageString];
@@ -83,13 +129,33 @@
         [selfBlock->_lbLanguageString setFrame:rctLanguageString];
         
         NSRect rctLanguage = selfBlock->_cbLanguage.frame;
-        rctLanguage.origin.x = 100;
+        rctLanguage.origin.x = 140;
         rctLanguage.origin.y = NSMinY(rctLanguageString);
         [selfBlock->_cbLanguage setFrame:rctLanguage];
+        
+        NSRect rctAnnounceTypeString = selfBlock->_lbAnnounceTypeString.frame;
+        rctAnnounceTypeString.origin.x = 20;
+        rctAnnounceTypeString.origin.y = NSMaxY(rctLanguageString) + 10;
+        [selfBlock->_lbAnnounceTypeString setFrame:rctAnnounceTypeString];
+        
+        NSRect rctAnnounceType = selfBlock->_cbAnnounceType.frame;
+        rctAnnounceType.origin.x = NSMinX(rctLanguage);
+        rctAnnounceType.origin.y = NSMinY(rctAnnounceTypeString);
+        [selfBlock->_cbAnnounceType setFrame:rctAnnounceType];
+        
+        NSRect rctAnnounceRuleString = selfBlock->_lbAnnounceRuleString.frame;
+        rctAnnounceRuleString.origin.x = 20;
+        rctAnnounceRuleString.origin.y = NSMaxY(rctAnnounceTypeString) + 10;
+        [selfBlock->_lbAnnounceRuleString setFrame:rctAnnounceRuleString];
+        
+        NSRect rctAnnounceRule = selfBlock->_cbAnnounceRule.frame;
+        rctAnnounceRule.origin.x = NSMinX(rctLanguage);;
+        rctAnnounceRule.origin.y = NSMinY(rctAnnounceRuleString);
+        [selfBlock->_cbAnnounceRule setFrame:rctAnnounceRule];
 
         NSRect rctAutoBlock = selfBlock->_cb24HourMode.frame;
         rctAutoBlock.origin.x = 20;
-        rctAutoBlock.origin.y = NSMaxY(rctLanguageString) + 10;
+        rctAutoBlock.origin.y = NSMaxY(rctAnnounceRuleString) + 10;
         [selfBlock->_cb24HourMode setFrame:rctAutoBlock];
         
         NSRect rctLaunchAtLogin = selfBlock->_cbLaunchAtLogin.frame;
@@ -105,26 +171,37 @@
     [_cbLaunchAtLogin setTitle:XUILocalizedString(@"launch_at_login")];
     [_cb24HourMode setTitle:XUILocalizedString(@"24_hour_mode")];
     [_lbLanguageString setText:[NSString stringWithFormat:@"%@:",XUILocalizedString(@"language")]];
+    [_lbAnnounceRuleString setText:[NSString stringWithFormat:@"%@:",XUILocalizedString(@"announce_rule")]];
+    [_lbAnnounceTypeString setText:[NSString stringWithFormat:@"%@:",XUILocalizedString(@"announce_type")]];
     [self.window setTitle:XUILocalizedString(@"preferences")];
+    
+    _arrayAnnounceRules = @[XUILocalizedString(@"announce_hourly"),XUILocalizedString(@"announce_half_hourly"),XUILocalizedString(@"announce_quarter_hourly"),XUILocalizedString(@"announce_by_hand")];
+    [_cbAnnounceRule removeAllItems];
+    for(NSString *rule in _arrayAnnounceRules){
+        [_cbAnnounceRule addItemWithObjectValue:rule];
+    }
+    [_cbAnnounceRule selectItemAtIndex:[[HAFConfigureManager sharedManager] announceRule]];
 }
 
--(void)awakeFromNib{
-    [self.window setBackgroundColor:[NSColor colorWithCalibratedRed:250/255.0 green:250/255.0 blue:250/255.0 alpha:0.99]];
-    [[self.window standardWindowButton:NSWindowMiniaturizeButton] setHidden:YES];
-    [[self.window standardWindowButton:NSWindowZoomButton] setHidden:YES];
-    [self.window setContentView:_mainView];
-    [_mainView layoutSubviews];
+-(void)__updateUI{
+    [_cbLaunchAtLogin setState:[[HAFConfigureManager sharedManager] isStartup]];
+    [_cb24HourMode setState:[[HAFConfigureManager sharedManager] isTwentyfourHour]];
+    [_cbAnnounceType selectItemAtIndex:[[HAFConfigureManager sharedManager] announceType]];
+    [_cbAnnounceRule selectItemAtIndex:[[HAFConfigureManager sharedManager] announceRule]];
 }
+
+#pragma mark - Public methods
 
 -(IBAction)startup_click:(id)sender{
     [[HAFConfigureManager sharedManager] setStartup:NSOnState == [_cbLaunchAtLogin state]];
 }
 
--(IBAction)autoBlock_click:(id)sender{
+-(IBAction)twentyfourMode_click:(id)sender{
     [[HAFConfigureManager sharedManager] setTwentyfourHour:NSOnState == [_cb24HourMode state]];
 }
 
 -(void)showWindow:(id)sender{
+    [self __updateUI];
     [self.window setLevel:NSStatusWindowLevel];
     [self.window center];
     [[NSApplication sharedApplication] activateIgnoringOtherApps:YES];
@@ -132,8 +209,21 @@
 }
 
 - (void)comboBoxSelectionDidChange:(NSNotification *)notification{
-    NSString *languageCode = [_arrayLanguagesCode objectAtIndex: [_cbLanguage indexOfSelectedItem]];
-    [[XUILanguageManager sharedManager] setLanguage:languageCode];
+    if (nil == [notification object]) {
+        return;
+    }
+    NSUInteger index = [[notification object] indexOfSelectedItem];
+    if (-1 == index) {
+        return;
+    }
+    if (_cbLanguage == [notification object]) {
+        NSString *languageCode = [_arrayLanguagesCode objectAtIndex: [_cbLanguage indexOfSelectedItem]];
+        [[XUILanguageManager sharedManager] setLanguage:languageCode];
+    }else if(_cbAnnounceRule == [notification object]){
+        [[HAFConfigureManager sharedManager] setAnnounceRule:index];
+    }else if(_cbAnnounceType == [notification object]){
+        [[HAFConfigureManager sharedManager] setAnnounceType:index];
+    }
 }
 
 @end
